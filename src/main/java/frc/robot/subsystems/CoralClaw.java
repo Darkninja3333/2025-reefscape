d90a04dd766d46ab14c4d5dff3152d32edf1e682
Laurent <sopl17a1@eleve.csrs.qc.ca>
2025-01-13 17:41:08 -0500

    Created the neo for up and down movement of the claw

--- src/main/java/frc/robot/subsystems/CoralClaw.java
+++ src/main/java/frc/robot/subsystems/CoralClaw.java
@@ -5,14 +5,16 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import com.revrobotics.spark.SparkMax;
 /** 2 neo one controls elevation and one closes pinchers * */
 public class CoralClaw extends SubsystemBase {
   // pour les neo utiliser revlib et spark max ex: private CANSparkMax m_exemple = new
   // CANSparkMax(kidexemple);
+  SparkMax m_ElevationNeo = new SparkMax(0, SparkMax.MotorType.kBrushless);
   public CoralClaw() {
 // initialize the NEOs of/and the Claw of the robot. :D
-  }
+    
+}
 
   @Override
   public void periodic() {
