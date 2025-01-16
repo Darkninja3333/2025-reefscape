a3c25d5315677007cca7a2a29515369b51f06311
Laurent <sopl17a1@eleve.csrs.qc.ca>
2025-01-16 17:34:00 -0500

    Saved my things because of an unknown problem

--- src/main/java/frc/robot/subsystems/CoralClaw.java
+++ src/main/java/frc/robot/subsystems/CoralClaw.java
@@ -4,21 +4,23 @@
 
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.Degree;
-
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+//import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.AlgaeCoralStand;
+//import frc.robot.Constants.AlgaeCoralStand;
 
 /** 2 neo one controls elevation and one closes pinchers * */
 public class CoralClaw extends SubsystemBase {
-  // pour les neo utiliser revlib et spark max ex: private CANSparkMax m_exemple = new
+  // pour les neo utiliser revlib et spark max ex: private CANSparkMax m_exemple =
+  // new
   // CANSparkMax(kidexemple);
   private SparkMax m_ElevationNeo = new SparkMax(0, MotorType.kBrushless);
   private SparkMax m_PinchNeo = new SparkMax(1, MotorType.kBrushless);
-  
+  private RelativeEncoder m_ElevationEncoder = m_ElevationNeo.getEncoder();
+
   public void IntakeSetAngle(double angle) {
     m_ElevationNeo.set(angle);
   }
@@ -27,16 +29,13 @@ public class CoralClaw extends SubsystemBase {
     m_PinchNeo.set(speed);
   }
 
-  @Override
-  public void periodic() {
-    
+  if(m_ElevationEncoder.getPosition()){
+    m_PinchNeo.set(1);
   }
 
 public void intake() {
-  // make a command that could intake the coral. :)
-  if (Degree == 0) {
-    m_ElevationNeo.getangle(90);
-    m_PinchNeo.set(50);
-  }
+  // make a command that could intake the coral. :) 
+  m_ElevationEncoder.getPosition();
+  m_PinchNeo.get();
 }
 }
